72dbf407ce5d55908dcfb19b4bcd18d3db688816
Yun4g <delightvincent487@gmail.com>
2025-07-03 10:19:10 +0100

    update error

--- src/index.js
+++ src/index.js
@@ -23,7 +23,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use(loggerMiddleware)
+// app.use(loggerMiddleware) 
+
 app.use(AuthMiddleWare)
 app.use(errorHandler)
 
